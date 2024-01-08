ba20eda86eea8660103482f65bbb1a30f9645801
Djordje Kovacevic <djordjekovacevicsocials@gmail.com>
2024-01-08 20:22:52 +0100

    Start.

---
+++ src/main/java/com/Djole/SkillNetworkAPI/SkillNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.Djole.SkillNetworkAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillNetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Djole/SkillNetworkAPI/SkillNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Djole.SkillNetworkAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
